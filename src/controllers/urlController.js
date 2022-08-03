b80147847d9cfdd4ffc2404d2e4af34463cc9c54
Darlon Gomes <gomesdarlon@icloud.com>
2022-08-03 19:35:24 -0300

    chore: make urlSearch controller

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -22,6 +22,18 @@ export const urlShortener = async (req, res) => {
 }
 
 export const urlSearcher = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const {rows: body} = await connection.query(`
+        SELECT id, "shortUrl", url 
+        FROM urls 
+        WHERE id = $1
+        `,[id]);
+        if(body.length === 0) return res.sendStatus(404);
+        return res.send(body[0]).status(200);
+    } catch (error) {
+        return res.sendStatus(500);
+    }
 
 };
 
