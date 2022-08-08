1c3f1f550584288e2575b378cd356d82f216f322
Darlon Gomes <gomesdarlon@icloud.com>
2022-08-08 11:31:42 -0300

    fix: schema wasn't showing error

--- src/middlewares/schemaMiddleware.js
+++ src/middlewares/schemaMiddleware.js
@@ -7,15 +7,16 @@ export default function schemaValidation (req, res, next){
     switch (true) {
         case data.confirmPassword != undefined:
             validation = signUpSchema.validate(data, {abortEarly: false});
-            if(validation.error) return res.send(validation.error).status(422);
+           
+            if(validation.error) return res.status(422).send(validation.error);
             break;
         case data.url !== undefined:
             validation = shortenSchema.validate(data, {abortEarly: false});
-            if(validation.error) return res.send(validation.error).status(422);
+            if(validation.error) return res.status(422).send(validation.error);
             break;
         default:
             validation = signInSchema.validate(data, {abortEarly: false});
-            if(validation.error) return res.send(validation.error).status(422);
+            if(validation.error) return res.status(422).send(validation.error);
             break;
     }
     next();
